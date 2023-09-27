700800e4b536e9ec3a177b814ba5a257f39c770f
TimKroeger13 <t.kroeger13@googlemail.com>
2023-09-27 12:32:29 +0200

    Removing leftovers

--- Program.cs
+++ Program.cs
@@ -22,25 +22,5 @@ internal static class Program
         var DownloadAndUpdateGMLs = provider.GetRequiredService<IDownloadAndUpdateGMLs>();
 
         DownloadAndUpdateGMLs.UpdateDownloadGMLs();
-
-
-
-        //remoteFileExsists.RemoteFileExists("https://fbinter.stadt-berlin.de/fb/wfs/data/senstadt/s_poly_entzugspot2400_100?service=wfs&version=2.0.0&request=GetFeature&typeNames=fis:s_poly_entzugspot2400_100");
-
-        //var xml = XDocument.Load("https://fbinter.stadt-berlin.de/fb/wfs/data/senstadt/s_poly_entzugspot2400_100?service=wfs&version=2.0.0&request=GetFeature&typeNames=fis:s_poly_entzugspot2400_100");
-
-        //xml.Save("V:/Repos/GERMAG_local/GERMAG_local/Resources/testOutput.gml");
-
-        //string directory = Directory.GetCurrentDirectory();
-
-
-
-        //Console.WriteLine("Hello, World!");
-
     }
-
-
-
-
-
 }
